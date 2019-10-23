4d4b0a7336afbeaef0d3c06f2034d6b3e4598c23
kowalski <darkkowalski2012@gmail.com>
2019-10-23 22:48:13 +0800

    update: modify some names

--- test/test_bench/test_bench.v
+++ test/test_bench/test_bench.v
@@ -2,18 +2,18 @@
 
 module test_bench();
 
-    reg CLOCK;
+    reg CLOCK_50;
     reg rst;
 
     initial begin
         CLOCK = 1'b0;
-        forever #10 CLOCK = ~CLOCK // 50 MHz
+        forever #10 CLOCK_50 = ~CLOCK_50 // 50 MHz
     end
 
     // TODO: adjust durations
     initial begin
         rst = 1'b1; // set rst signal
-        #195 rst = 1'b0; // reset rst signal after 195 time units
+        #185 rst = 1'b0; // reset rst signal after 195 time units
         #2000 $stop; // stop simulation after 2000 time units
     end
 
